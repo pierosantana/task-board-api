a899ed91ebf710b41e0558fe3918584abadc2719
pierosantana <piero.santanalarosa@gmail.com>
2026-02-15 02:24:06 +0100

    rename url's in Controller's

--- src/main/java/com/psltasks/controller/ProjectController.java
+++ src/main/java/com/psltasks/controller/ProjectController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/projects")
+@RequestMapping("/projects")
 public class ProjectController {
 
     private final ProjectService projectService;
